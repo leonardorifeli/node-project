e5c3b9fabb54927b0218d7398f785fd288c32f95
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-11-05 18:18:15 -0200

    Changed the express config

--- config/express.js
+++ config/express.js
@@ -1,9 +1,14 @@
 var express = require("express");
+var home = require("../app/routes/home");
 
 module.exports = function(){
     var app = express();
-    
+
+    home(app);
+
     app.set("port", 3000);
+    app.set("view engine", "ejs");
+    app.set("views", "./app/views");
     app.use(express.static("./public"));
 
     return app;
