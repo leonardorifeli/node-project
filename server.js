6b57e25dfc415d9c4b6660a2b700b6c16854e948
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-11-05 14:10:27 -0200

    Changed the server.js - Added the express

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
-http.createServer(function(req,res){
-    res.writeHead(200, {'content-type': 'text/plain'});
-    res.end("Node server!!");
-}).listen(3000, '127.0.0.1');
+var app = require("./config/express")();
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server on "+app.get('port'));
+});
