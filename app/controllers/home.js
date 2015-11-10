f00ef2e6e7208cba5d1ae87f68fabcb4852169b7
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-11-10 14:42:21 -0200

    Changed home controller

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,9 @@
 module.exports = function(){
     var controller = {};
+    
     controller.index = function(req, res){
         res.render("index", {name: "Leonardo Rifeli"})
     };
+
     return controller;
 }
